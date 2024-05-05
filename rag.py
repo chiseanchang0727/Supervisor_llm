7e4860bd0af24f0e8339ca7e7f04f9a7a366dbf8
chiseanchang0727 <luciuskyear0812@gmail.com>
2024-05-05 19:25:51 +0800

    update

--- rag.py
+++ rag.py
@@ -1,6 +1,7 @@
 from langchain import hub
 from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
@@ -13,11 +14,19 @@ configs = read_json('configs.json')
 DATA_PATH = configs["DATA_PATH"]
 DB_PATH = configs['DB_PATH']
 
-QA_CHAIN_PROMPT = hub.pull("rlm/rag-prompt-mistral")
+# modify the RAG prompt here
+#QA_CHAIN_PROMPT = hub.pull("rlm/rag-prompt-llama")
+
+QA_CHAIN_PROMPT = ChatPromptTemplate.from_messages(
+  ("human", """[INST]<<SYS>> You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.<</SYS>> 
+Question: {question} 
+Context: {context} 
+Answer: [/INST]""")
+)
 
 def load_llm():
     llm = Ollama(
-        model = 'mistral',
+        model = 'llama3',
         verbose = 'True',
         callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
     )
